8528b9c9962e24a95474f324be225eb7e1550632
jhonben94 <jhonyben.94@gmail.com>
2023-12-06 08:02:01 -0300

    Mod

--- src/app/demo/components/entidades/estudiantes/estudiante-edit/estudiante-edit.component.ts
+++ src/app/demo/components/entidades/estudiantes/estudiante-edit/estudiante-edit.component.ts
@@ -2,75 +2,62 @@ import { CursosService } from './../../../../service/cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-estudiante-edit',
-  templateUrl: './estudiante-edit.component.html',
-  styleUrls: ['./estudiante-edit.component.scss']
+    selector: 'app-estudiante-edit',
+    templateUrl: './estudiante-edit.component.html',
+    styleUrls: ['./estudiante-edit.component.scss'],
 })
-export class EstudianteEditComponent implements OnInit{
+export class EstudianteEditComponent implements OnInit {
+    listaCursos: any[] = [];
+    listaSecciones: any[] = [];
+    cursoSeleccionado: any;
+    seccionSeleccionada: any = null;
+    estudianteData:any ={ }
+    generoList = [
+        { name: 'Masculino', code: 'M' },
+        { name: 'Femenino', code: 'F' },
+    ];
 
-  listaCursos: any[] = []
-  listaSecciones: any[] = []
-  cursoSeleccionado : any ;
-  selectedState: any = null;
+    constructor(public service: CursosService) {}
 
-  states: any[] = [
-      {name: 'Arizona', code: 'Arizona'},
-      {name: 'California', value: 'California'},
-      {name: 'Florida', code: 'Florida'},
-      {name: 'Ohio', code: 'Ohio'},
-      {name: 'Washington', code: 'Washington'}
-  ];
+    ngOnInit(): void {
+        this.service.obtenerPorDocente(1, 2).subscribe({
+            next: (data: any[]) => {
+                this.listaCursos = data;
+            },
+        });
+    }
+    obtenerSecciones() {
+        console.log('Seccionado');
+        console.log(this.cursoSeleccionado);
+        this.listaSecciones = this.cursoSeleccionado.seccionList;
+    }
+    onChange(event) {
+       
+        const listaSecciones = event.value.seccionList;
+        this.listaSecciones = listaSecciones;
+    }
+    guardar(){
+     
+       const {
+        nombre,
+        apellido,
+        edad,
+        sexo
+      } = this.estudianteData;
 
-  dropdownItems = [
-      { name: 'Option 1', code: 'Option 1' },
-      { name: 'Option 2', code: 'Option 2' },
-      { name: 'Option 3', code: 'Option 3' }
-  ];
-  generoList = [
-      { name: 'Masculino', code: 'M' },
-      { name: 'Femenino', code: 'F' },
-  ];
-
-  cities1: any[] = [];
-
-  cities2: any[] = [];
-
-  city1: any = null;
-
-  city2: any = null;
-
-  constructor(public service: CursosService) { }
-
-  ngOnInit(): void {
-     this.service.obtenerPorDocente(1,2).subscribe({
-      next:(data: any[])=>{
-
-        /* 
-        [
-          {
-              "idCurso": 2,
-              "codigo": "T1",
-              "anio": 2023,
-              "descripcion": "TALLER 1",
-              "seccionList": [
-                  {
-                      "idSeccion": 4,
-                      "codigo": "A",
-                      "descripcion": "PRIMERA SECCION"
-                  }
-              ]
-          }
-      ]
-              
-        */
-        console.log(data)
-        this.listaCursos = data;
+      const {code} = sexo;
+      const {idCurso} =this.cursoSeleccionado;
+      const {idSeccion} =this.seccionSeleccionada;
+      
+      const data={
+        nombre,
+        apellido,
+        edad,
+        sexo:code,
+        idCurso,
+        idSeccion
       }
-     }) 
-  }
-  obtenerSecciones(){
-    console.log(this.cursoSeleccionado);
-    
-    this.listaSecciones = this.cursoSeleccionado.seccionList
-  }
+      console.log(data)
+
+    }
 }
